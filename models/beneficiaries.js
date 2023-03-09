4529a68731499b82411459171970b8c36d348573
ManeDM <mane657@hotmail.com.com>
2023-03-09 12:04:40 -0500

    fixing routes

--- models/beneficiaries.js
+++ models/beneficiaries.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const basicInfoSchema = require("./beneficiaries/basicInfoSchema");
 const birthInfoSchema = require("./beneficiaries/birthinformationSchema");
+const healthSchema = require('./beneficiaries/healthSchema');
 
 const beneficiarySchema = new Schema({
     basicinfo: basicInfoSchema,
-    birthinformation: birthInfoSchema
+    birthinformation: birthInfoSchema,
+    healthInfo: healthSchema
 });
 
 const beneficiary = mongoose.model('beneficiary', beneficiarySchema);
