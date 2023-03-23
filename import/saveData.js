e10aeae7272b780065f5189c3811228a86e177a5
Chris <cgolayac@correo.udistrital.edu.co>
2023-03-22 21:08:52 -0500

    import backend mvp finish

--- import/saveData.js
+++ import/saveData.js
@@ -3,92 +3,36 @@ const beneficiary = require('./parsers/beneficiary');
 const index = require('./parsers/index');
 const { initGlobalVars, globalVars } = require('./parsers/controllers/globalConst');
 
-
-
 const saveBasicInfoData = async (path) => {
     initGlobalVars(path);
     let failedBeneficiaries = [];
-    for (row = globalVars.row; row <= globalVars.lastRow-1; row++){
+    const totalRows = globalVars.lastRow - globalVars.row;
+    let percent = '';
+    for (let row = globalVars.row; row <= globalVars.lastRow-1; row++){
         index(row);
         try {
-            // Busca si ya existe un beneficiario con el mismo número de identificación
             const existingBeneficiary = await Beneficiary.find({ 'basicinfo.numDoc': beneficiary.basicinfo.numDoc });
-            // Si ya existe, salta este registro y continúa con el siguiente
             if (existingBeneficiary.length > 0) {
                 console.log(`Beneficiary with identification number ${existingBeneficiary[0].basicinfo.numDoc} already exists, skipping...`);
                 failedBeneficiaries.push({ numDoc: existingBeneficiary[0].basicinfo.numDoc, error: 'Beneficiary already exists' });
                 continue;
             };   
-            // Si no existe, crea un nuevo registro
             const savedBeneficiary = await Beneficiary.create(beneficiary);
             /*console.log('Datos insertados:', savedBeneficiary);*/
         } catch (error) {
-            // Maneja el error si se produce alguna excepción durante el proceso
-            console.log('Error:', error.message);
+            // console.log('Error:', error.message);
             failedBeneficiaries.push({ numDoc: beneficiary.basicinfo.numDoc, error: error.message });
         }
+
+        // Calcular y mostrar porcentaje completado
+        const percentCompleted = ((row - globalVars.row) / totalRows) * 100;
+        percent = `${percentCompleted.toFixed(1)}%`
+        console.log(percent)
     }
     console.log('Todo ha sido un exito');
     console.log(failedBeneficiaries);
+    return percent;
+    
 };
-
-
 
 module.exports = saveBasicInfoData;
-/*module.exports = saveData;*/
-
-/*const saveBasicInfoData = (path) => {
-// const saveBasicInfoData = (path) => {
-//     initGlobalVars(path);
-//     for (row = globalVars.row; row <= globalVars.lastRow-1; row++){
-//         index(row);
-//         Beneficiary.create(beneficiary).then((result) => {
-//         // console.log('Datos insertados:', result);
-//         }).catch((err) => {
-//             if (err.code === 11000) {
-//                 // Beneficiario duplicado
-//                 console.log('Error: Beneficiario duplicado');
-//                 console.log('Datos del beneficiario:', beneficiary);
-//             } else if (err.name === 'ValidationError') {
-//                 // Beneficiario con datos inválidos
-//                 console.log('Error: Beneficiario con datos inválidos');
-//                 console.log('Mensajes de validación:', err.errors);
-//                 console.log('Datos del beneficiario:', beneficiary);
-//             } else {
-//                 console.log('Error desconocido:', err);
-//             }
-//         });
-//     }
-// };
-
-
-const saveData = async (path) => {
-    initGlobalVars(path);
-    let failedBeneficiaries = [];
-    for (row = globalVars.row; row <= globalVars.lastRow-1; row++){
-        index(row);
-        try {
-            // Busca si ya existe un beneficiario con el mismo número de identificación
-            const existingBeneficiary = await Beneficiary.find({ 'basicinfo.numDoc': beneficiary.basicinfo.numDoc });
-            // Si ya existe, salta este registro y continúa con el siguiente
-            if (existingBeneficiary.length > 0) {
-                console.log(`Beneficiary with identification number ${existingBeneficiary[0].basicinfo.numDoc} already exists, skipping...`);
-                failedBeneficiaries.push({ numDoc: existingBeneficiary[0].basicinfo.numDoc, error: 'Beneficiary already exists' });
-                continue;
-            };   
-            // Si no existe, crea un nuevo registro
-            const savedBeneficiary = await Beneficiary.create(beneficiary);
-            console.log('Datos insertados:', savedBeneficiary);
-        } catch (error) {
-            // Maneja el error si se produce alguna excepción durante el proceso
-            console.log('Error:', error.message);
-            failedBeneficiaries.push({ numDoc: beneficiary.basicinfo.numDoc, error: error.message });
-        }
-    }
-};
-    console.log('Todo ha sido un exito');
-    console.log(failedBeneficiaries);*/
-
-
-
-
--- routes/import/import.routes.js
+++ routes/import/import.routes.js
@@ -3,21 +3,25 @@ const saveData = require('../../import/saveData');
 const fs = require('fs').promises;
 const path = require('path');
 
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 // Endpoint Create Beneficiarie
-router.post("/", async (req, res)=>{
+router.post("/",upload.single('archivo'), async (req, res)=>{
     try {
         // Guarda el archivo temporalmente en el servidor
-        const file = req.files.file;
-        const filePath = path.join(__dirname, 'temp', file.name);
-        await fs.writeFile(filePath, file.data);
+        const file = req.file;
+        const filePath = path.join(__dirname, file.originalname);
+        await fs.writeFile(filePath, file.buffer);
 
         // Utiliza la función saveData con la ruta del archivo temporal
         await saveData(filePath);
-
         // Borra el archivo temporal del servidor
         await fs.unlink(filePath);
 
-        res.status(200).send('Datos importados correctamente');
+        res.status(200).json({message:'Datos importados correctamente'});
     } catch (error) {
         console.log(error)
         res.status(500).json({ message: error.message });
